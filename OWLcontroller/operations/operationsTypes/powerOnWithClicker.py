4837a94bbbfaa49272b5d9fa3f0e5fc3c8a9fa71
Elad <elad1593210@gmail.com>
2021-01-15 15:17:46 +0200

    Activate the clicker with python

--- OWLcontroller/operations/operationsTypes/powerOnWithClicker.py
+++ OWLcontroller/operations/operationsTypes/powerOnWithClicker.py
@@ -1,2 +1,37 @@
+import os
+import subprocess
+import time
+
+from operations.operationsTypes.runCommandViaCmd import runCommandViaCmd
+CMD_COMMAND = 'cmd /k '
+
 class powerOnWithClicker():
-    pass
+    def getKey(self):
+        ''' Returns operation's name '''
+        return (type(self).__name__)
+    @staticmethod
+    def runOp():
+        # os.system(CMD_COMMAND + 'mode COM4 BAUD=9600 PARITY=n DATA=8')
+
+        # time.sleep(1)
+        #os.system(CMD_COMMAND + 'echo 1 > COM4')
+        # # os.system(CMD_COMMAND + 'ipconfig')
+        #os.system(CMD_COMMAND + 'echo q > COM4')
+        print("done")
+        #os.system(CMD_COMMAND + 'timeout 1')
+        # os.system("start /B start cmd.exe @cmd /k echo 1 > COM4")
+        # os.system("start /B start cmd.exe @cmd /k echo q > COM4")
+
+print (powerOnWithClicker.runOp())
+
+
+#
+# mode COM4 BAUD=9600 PARITY=n DATA=8
+# echo 1 > COM4
+# timeout 1
+# echo q > COM4
+# timeout 1
+
+
+
+
