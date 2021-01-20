2559938938a31bfc1ae7d5769be7c0939d120641
Elad <elad1593210@gmail.com>
2021-01-20 12:58:23 +0200

    shutdown remote PC

--- OWLhostPC/operations/operationsTypes/shutdown.py
+++ OWLhostPC/operations/operationsTypes/shutdown.py
@@ -12,5 +12,5 @@ class shutdown(object):
     @staticmethod
     def runOp(userCommand):
         print(userCommand)
-        #return userCommand in case we are checking it on the same pc (in order to nu shoutdown the PC)
+        #return userCommand
         os.system("shutdown /s /t 1")
